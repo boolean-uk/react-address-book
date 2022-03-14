5f0d1c79edf95273a8d3668de99312d4a9816a14
BrandonV21 <brandon00095@gmail.com>
2022-03-14 10:15:27 +0000

    React Address Book

--- src/components/ContactsList.js
+++ src/components/ContactsList.js
@@ -1,10 +1,13 @@
 import { useState } from "react"
-import { Link, useSearchParams } from "react-router-dom"
+import { Link, Route, Routes, useSearchParams } from "react-router-dom"
+import ContactsView from "./ContactsView"
 
 function ContactsList(props) {
   
   //"contacts" must be passed as prop to this component
   const { contacts } = props
+  // console.log("Log contact list", props)
+  console.log("log contact list", props)
 
   return (
     <>
@@ -15,15 +18,19 @@ function ContactsList(props) {
         {contacts.map((contact, index) => {
           const { firstName, lastName } = contact
           return (
+            
             <li className="contact" key={index}>
               <p>
                 {firstName} {lastName}
               </p>
               <p>
                 { /** TODO: Make a Link here to view contact */}
+                <Link to={`/contacts/${contact.id}`}>
                 View
+                </Link>
               </p>
             </li>
+            
           )
         })}
       </ul>
