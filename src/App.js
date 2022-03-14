13521b6315cdaf054488daf145f4a6536f8269af
Safia <safia.ibrahim2121@gmail.com>
2022-03-14 09:47:50 +0000

    final push

--- src/App.js
+++ src/App.js
@@ -1,30 +1,60 @@
-import { useEffect, useState } from "react"
-import { Link, Route, Routes } from "react-router-dom"
-import ContactsList from "./components/ContactsList"
-import ContactsAdd from "./components/ContactsAdd"
-import ContactsView from "./components/ContactsView"
-import "./styles/styles.css"
+import { useEffect, useState } from "react"; // We're importing both the useEffect and the UseState functions from react 'engine'
+import { Link, Route, Routes } from "react-router-dom"; // importing another react elements (Link, Route and Routes), I think these are element and not functions as they start with uppercase letter - just guessing.
+import ContactsList from "./components/ContactsList"; // in line 3,4 and 5, we're importing the elements that we've created.
+import ContactsAdd from "./components/ContactsAdd"; //
+import ContactsView from "./components/ContactsView"; //
+import "./styles/styles.css"; // importing the css style sheet.
 
 export default function App() {
-  const [contacts, setContacts] = useState([])
-  
+  //
+  const [contacts, setContacts] = useState([]); // destructering the useState func and setting an initial state (empty array)
+
   //TODO: Load all contacts on useEffect when component first renders
 
+  useEffect(() => {
+    // begining of the useEffect function
+    fetch("http://localhost:4000/contacts") // we're making a request to API
+      .then((res) => res.json()) // the response will then be transformed to JS object
+      .then((json) => {
+        // this function is caring the dating returned in the JS obj
+        console.log(json); // this gives you a visibility of the data received
+        setContacts(json); // this line is setting/updating the setfunction to the new data
+      });
+  }, []); // end of the first argument of the useEffect func (}). The second arg is current an empty array as we only want the useEffect function to render the data once.
+
   return (
+    // here is the begining of the JSX that the App func is returning which is responsible of the content that gets displayed on the page
     <>
       <nav>
+        {" "}
         <h2>Menu</h2>
         <ul>
-          {/* TODO: Make these links */}
-          <li>Contacts List</li>
-          <li>Add New Contact</li>
+          <li>
+            {" "}
+            <Link to="/">Contacts List</Link>{" "}
+            {/*this li is renderig a text which is wrapped in the 'Link' element that we imported from react. the 'to' attribute , I think, is a keyword which you must use; it points to a directory */}
+          </li>
+          <li>
+            <Link to="/contacts/add">Add New Contact</Link>{" "}
+          </li>
         </ul>
       </nav>
       <main>
         <Routes>
-          {/* TODO: Add routes here  */}
+          {" "}
+          {/* This looks a parent element of the Route - I'm still not sure what it does exactly but I think I have an idea of it.*/}
+          <Route path="/" element={<ContactsList contacts={contacts} />} />
+          {/* I don't really know how to explain what this line is doing other than: the route eelement is using a path which is the same one used in the 'link' and that it has access to the ContactList component with its state. So when the link 'Contact List' is clicked, this 'route' gets activated and takes the user into the contact list?   */}
+          {/*  */}
+          <Route
+            path="/contacts/add"
+            element={
+              <ContactsAdd contacts={contacts} setContacts={setContacts} />
+            }
+          />
+          <Route path="contacts/:id" element={<ContactsView />} />
         </Routes>
       </main>
     </>
-  )
+  );
 }
--- src/components/ContactsList.js
+++ src/components/ContactsList.js
@@ -1,11 +1,9 @@
-import { useState } from "react"
-import { Link, useSearchParams } from "react-router-dom"
+import { useState } from "react";
+import { Link, useSearchParams } from "react-router-dom";
 
 function ContactsList(props) {
-  
-  //"contacts" must be passed as prop to this component
-  const { contacts } = props
-
+  const { contacts } = props; // We're destructuring the props passed on - this means we can use the name 'contacts' and it will be recognised as the state contacts.
+  console.log("contacts", contacts); // this console log can be used to check if you've passed the correct data on.
   return (
     <>
       <header>
@@ -13,22 +11,28 @@ function ContactsList(props) {
       </header>
       <ul className="contacts-list">
         {contacts.map((contact, index) => {
-          const { firstName, lastName } = contact
+          // this line is mapping through the contacts array
+          const { firstName, lastName } = contact; // I think here is another example of destructuring prop/objrct - we're taking out/using two propert of the state object (firstName and lastName).This means the variable will be recognised for the value they're holding/passing.
           return (
             <li className="contact" key={index}>
               <p>
-                {firstName} {lastName}
+                {firstName} {lastName}{" "}
+                {/* This line is responsible of displaying the value passed from the form to the conatct list page */}
               </p>
               <p>
-                { /** TODO: Make a Link here to view contact */}
-                View
+                {" "}
+                <Link to={`/contacts/${contact.id}`}>
+                  {" "}
+                  {/* This is the link that directs the viewers to the individual contact that they've clicked to view of their details, I think? */}
+                  View
+                </Link>
               </p>
             </li>
-          )
+          );
         })}
       </ul>
     </>
-  )
+  );
 }
 
-export default ContactsList
+export default ContactsList;
