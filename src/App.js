f488af352a24e3477535eed75ef27af8353e0ccd
sigristarisa <arisasigrist.ch@gmail.com>
2022-05-18 16:02:46 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { useEffect, useState } from "react"
-import { Link, Route, Routes } from "react-router-dom"
-import ContactsList from "./components/ContactsList"
-import ContactsAdd from "./components/ContactsAdd"
-import ContactsView from "./components/ContactsView"
-import "./styles/styles.css"
+import { useEffect, useState } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import ContactsList from "./components/ContactsList";
+import ContactsAdd from "./components/ContactsAdd";
+import ContactsView from "./components/ContactsView";
+import "./styles/styles.css";
 
-export default function App() {
-  const [contacts, setContacts] = useState([])
-  
+const App = () => {
+  const [contacts, setContacts] = useState([]);
+
   //TODO: Load all contacts on useEffect when component first renders
 
   return (
@@ -16,15 +16,20 @@ export default function App() {
         <h2>Menu</h2>
         <ul>
           {/* TODO: Make these links */}
-          <li>Contacts List</li>
+          <Link to="/">
+            <li>Contacts List</li>
+          </Link>
+
           <li>Add New Contact</li>
         </ul>
       </nav>
       <main>
         <Routes>
-          {/* TODO: Add routes here  */}
+          <Route path="/" element={ContactsList} />
         </Routes>
       </main>
     </>
-  )
-}
+  );
+};
+
+export default App;
