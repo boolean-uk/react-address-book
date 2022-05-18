4fa0d12176652bab3dffc447a18347a9a8464330
UnknownCreator1 <90321114+UnknownCreator1@users.noreply.github.com>
2022-05-18 16:33:17 +0100

    Contact List rendered

--- src/components/ContactsList.js
+++ src/components/ContactsList.js
@@ -1,10 +1,9 @@
-import { useState } from "react"
-import { Link, useSearchParams } from "react-router-dom"
+import { useState } from "react";
+import { Link, useSearchParams } from "react-router-dom";
 
 function ContactsList(props) {
-  
   //"contacts" must be passed as prop to this component
-  const { contacts } = props
+  const { contacts } = props;
 
   return (
     <>
@@ -13,22 +12,22 @@ function ContactsList(props) {
       </header>
       <ul className="contacts-list">
         {contacts.map((contact, index) => {
-          const { firstName, lastName } = contact
+          const { firstName, lastName } = contact;
           return (
             <li className="contact" key={index}>
               <p>
                 {firstName} {lastName}
               </p>
               <p>
-                { /** TODO: Make a Link here to view contact */}
-                View
+                {/** TODO: Make a Link here to view contact */}
+                <Link to={`/contacts/${contact.id}`}>View</Link>
               </p>
             </li>
-          )
+          );
         })}
       </ul>
     </>
-  )
+  );
 }
 
-export default ContactsList
+export default ContactsList;
