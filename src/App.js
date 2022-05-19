533e8026142fd6fc803f7476bd0e9ac83aa8d60d
Anica Rahman <anicarahman2009@yahoo.it>
2022-05-19 16:35:12 +0100

    Beginning

--- src/App.js
+++ src/App.js
@@ -11,24 +11,37 @@ export default function App() {
   const [contacts, setContacts] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:4000/contacts")
-      .then((res) => res.json())
-      .then((data) => setContacts(data.results));
+    fetchContacts();
   }, []);
+
+  const fetchContacts = async () => {
+    const data = await fetch("http://localhost:4000/contacts");
+    const contacts = await data.json();
+    setContacts(contacts);
+  };
 
   return (
     <>
       <nav>
         <h2>Menu</h2>
         <ul>
-          <Link to="/">Contacts List</Link>
-          <Link to="/">Add New Contact</Link>
+          <li>
+            <Link to="/">Contacts List</Link>
+          </li>
+          <li>
+            <Link to="/contacts/add">Add New Contact</Link>
+          </li>
         </ul>
       </nav>
       <main>
         <Routes>
-          <Route element={<ContactsList />} />
-          <Route path="/" element={<ContactsAdd />} />
+          <Route path="/" element={<ContactsList contacts={contacts} />} />
+          <Route
+            path="/contacts/add"
+            element={
+              <ContactsAdd contacts={contacts} setContacts={setContacts} />
+            }
+          />
         </Routes>
       </main>
     </>
