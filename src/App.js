90f583837e8f057cc394e7fe2ac746a664ebf694
Alex Jennings <aj0312@my.bristol.ac.uk>
2022-05-20 10:43:09 +0200

    can edit contact

--- src/App.js
+++ src/App.js
@@ -1,30 +1,58 @@
-import { useEffect, useState } from "react"
-import { Link, Route, Routes } from "react-router-dom"
-import ContactsList from "./components/ContactsList"
-import ContactsAdd from "./components/ContactsAdd"
-import ContactsView from "./components/ContactsView"
-import "./styles/styles.css"
+import { useEffect, useState } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import ContactsList from "./components/ContactsList";
+import ContactsAdd from "./components/ContactsAdd";
+import ContactsView from "./components/ContactsView";
+import ContactsEdit from "./components/ContactsEdit";
+import "./styles/styles.css";
 
 export default function App() {
-  const [contacts, setContacts] = useState([])
-  
-  //TODO: Load all contacts on useEffect when component first renders
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/contacts`)
+      .then((res) => res.json())
+      .then((contacts) => setContacts(contacts));
+  }, []);
+
+  console.log(contacts);
 
   return (
     <>
       <nav>
         <h2>Menu</h2>
         <ul>
-          {/* TODO: Make these links */}
-          <li>Contacts List</li>
-          <li>Add New Contact</li>
+          <li>
+            <Link to="/contacts">Contacts List</Link>
+          </li>
+          <li>
+            <Link to="/add">Add New Contact</Link>
+          </li>
         </ul>
       </nav>
       <main>
         <Routes>
-          {/* TODO: Add routes here  */}
+          <Route
+            path="/contacts"
+            element={
+              <ContactsList contacts={contacts} setContacts={setContacts} />
+            }
+          />
+          <Route
+            path="/contacts/edit/:id"
+            element={
+              <ContactsEdit contacts={contacts} setContacts={setContacts} />
+            }
+          />
+          <Route
+            path="/add"
+            element={
+              <ContactsAdd setContacts={setContacts} contacts={contacts} />
+            }
+          />
+          <Route path="/contacts/view/:id" element={<ContactsView />} />
         </Routes>
       </main>
     </>
-  )
+  );
 }
