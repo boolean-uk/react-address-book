9d9049dc1f1ee75f84ac2b38e61a3258450f84d2
Joel Watkins <eck087@yahoo.co.uk>
2022-11-18 10:08:17 +0000

    displaying list of contacts

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import { useEffect, useState } from "react"
-import { Link, Route, Routes } from "react-router-dom"
-import ContactsList from "./components/ContactsList"
-import ContactsAdd from "./components/ContactsAdd"
-import ContactsView from "./components/ContactsView"
-import "./styles/styles.css"
+import { useEffect, useState } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import ContactsList from "./components/ContactsList";
+import ContactsAdd from "./components/ContactsAdd";
+import ContactsView from "./components/ContactsView";
+import "./styles/styles.css";
 
 export default function App() {
-  const [contacts, setContacts] = useState([])
-  
-  //TODO: Load all contacts on useEffect when component first renders
+  const [contacts, setContacts] = useState([]);
+
+  const getContacts = async () => {
+    const res = await fetch("http://localhost:4000/contacts");
+    const contacts = await res.json();
+    setContacts(contacts);
+  };
+
+  useEffect(() => {
+    getContacts();
+  }, []);
+
+  console.log(contacts);
 
   return (
     <>
@@ -22,9 +32,9 @@ export default function App() {
       </nav>
       <main>
         <Routes>
-          {/* TODO: Add routes here  */}
+          <Route path="/" element={<ContactsList contacts={contacts} />} />
         </Routes>
       </main>
     </>
-  )
+  );
 }
