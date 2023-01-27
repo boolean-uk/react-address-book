0d777b8bc65e6129c53b480216b1475e00b1f47f
Kat <katdemey@outlook.com>
2023-01-27 15:37:31 +0000

    created working links for viewing contact list and new contact form. Working on saving the submitted data to the form state to then add to the contacts state... and then POST request

--- src/App.js
+++ src/App.js
@@ -7,16 +7,16 @@ import "./styles/styles.css"
 
 export default function App() {
   const [contacts, setContacts] = useState([])
-  
+
   //TODO: Load all contacts on useEffect when component first renders
 
   useEffect(() => {
     fetch("http://localhost:4000/contacts")
-    .then((res) => res.json())
-    .then((contactData) => {
-      setContacts(contactData)
-      console.log("contactData:", contactData)
-    })
+      .then((res) => res.json())
+      .then((contactData) => {
+        setContacts(contactData)
+        console.log("contactData:", contactData)
+      })
   }, [])
 
   return (
@@ -24,16 +24,22 @@ export default function App() {
       <nav>
         <h2>Menu</h2>
         <ul>
-          {/* TODO: Make these links */}
-          <li>Contacts List</li>
-          <li>Add New Contact</li>
+          <li>
+            <Link to="/">
+              Contacts List
+            </Link>
+          </li>
+          <li>
+            <Link to="/ContactsAdd">
+              Add New Contact
+            </Link>
+          </li>
         </ul>
       </nav>
       <main>
-        <ContactsList contacts={contacts}/>
         <Routes>
-          {/* TODO: Add routes here  */}#
-          <Route path="/" element={ContactsList} />
+          <Route path="/" element={<ContactsList contacts={contacts} />} />
+          <Route path="/ContactsAdd" element={<ContactsAdd setContacts={setContacts} contacts={contacts}/>} />
         </Routes>
       </main>
     </>
--- src/components/ContactsAdd.js
+++ src/components/ContactsAdd.js
@@ -1,5 +1,16 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom";
+
+const initialState = {
+  // id: "", {index + 1}
+  // name: "",
+  firstName: "",
+  lastName: "",
+  street: "",
+  city: "",
+  //see if I need to add Type, and required = true
+}
+
 
 function ContactsAdd(props) {
 
@@ -7,25 +18,94 @@ function ContactsAdd(props) {
   // to this component so new contacts can be added to the
   // state
   const { setContacts, contacts } = props
+  const [formState, setFormState] = useState(initialState)
 
   //TODO: Implement controlled form
   //send POST to json server on form submit
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    //POST request
+
+    // const opts = {
+    //   method: 'POST',
+    //   headers: {'content-type': 'newcontact/json' },
+    // //double check what needs to be stringified
+    //   body: JSON.stringify({ name: event.target[0].value })
+    // }
+
+    // fetch("http://localhost:3003/ContactsAdd", opts)
+    // .then(res => res.json())
+    // .then(data => {
+    //   contacts
+    // })
+  console.log("formState", formState)
+    //add new data (js taken from the tips page)
+    // setContacts([...contacts, formState ])
+    //reset the form
+    // setFormState(initialState)
+  }
+
+
+
+  //change the input from empty string to submitted info
+  const handleChange = (event) => {
+    //set targets to change
+    const name = event.target.name
+    const value = event.target.value
+
+    if (name === "firstName") {
+      setFormState({ ...formState, firstName: value })
+      console.log(formState.firstName)
+    }
+    if (name === "lastName") {
+      setFormState({ ...formState, lastName: value })
+    }
+    if (name === "street") {
+      setFormState({ ...formState, street: value })
+    }
+    if (name === "city") {
+      setFormState({ ...formState, city: value })
+    }
+
+  }
+
 
   return (
-    <form className="form-stack contact-form">
+    <form className="form-stack contact-form" onSubmit={handleSubmit}>
       <h2>Create Contact</h2>
 
       <label htmlFor="firstName">First Name</label>
-      <input id="firstName" name="firstName" type="text" required />
+      <input id="firstName"
+        name="firstName"
+        type="text"
+        value={formState.id}
+        required
+        onChange={handleChange} />
 
       <label htmlFor="lastName">Last Name:</label>
-      <input id="lastName" name="lastName" type="text" required/>
+      <input id="lastName"
+        name="lastName"
+        type="text"
+        value={formState.name}
+        required
+        onChange={handleChange} />
 
       <label htmlFor="street">Street:</label>
-      <input id="street" name="street" type="text" required/>
+      <input id="street"
+        name="street"
+        type="text"
+        value={formState.name}
+
+        required
+        onChange={handleChange} />
 
       <label htmlFor="city">City:</label>
-      <input id="city" name="city" type="text" required/>
+      <input id="city"
+        name="city"
+        type="text"
+        value={formState.name}
+        required
+        onChange={handleChange} />
 
       <div className="actions-section">
         <button className="button blue" type="submit">
@@ -35,5 +115,6 @@ function ContactsAdd(props) {
     </form>
   )
 }
+
 
 export default ContactsAdd
