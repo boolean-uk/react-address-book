1c5d51fae8bfe080775805619fe91940713dd9d0
MaybeFreak <michaelpope97@gmail.com>
2023-01-27 15:03:17 +0100

    Core and merge done...  maybe

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from 'react-dom/client'
 
