ee02197ceec6675768e19e260c5342340a4d8829
Oliver West <oliversimon.west@gmail.com>
2023-01-27 11:20:28 +0000

    added links and routes to App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,13 @@ export default function App() {
   const [contacts, setContacts] = useState([])
   
   //TODO: Load all contacts on useEffect when component first renders
+  useEffect(() => {
+    fetch("http://localhost:4000/contacts")
+    .then((res) => res.json())
+    .then((data) => {
+        setContacts(data)
+    })
+  }, [])
 
   return (
     <>
@@ -16,13 +23,17 @@ export default function App() {
         <h2>Menu</h2>
         <ul>
           {/* TODO: Make these links */}
-          <li>Contacts List</li>
-          <li>Add New Contact</li>
+          <li><Link to={`/`}>Contacts List</Link></li>
+          <li><Link to={`/contacts/add`}>Add New Contact</Link></li>
         </ul>
       </nav>
       <main>
         <Routes>
           {/* TODO: Add routes here  */}
+          <Route path='/' element={<ContactsList contacts={contacts} />}/>
+          <Route path='/contacts/add' element={<ContactsAdd contacts={contacts} setContacts={setContacts}/>}/>
+          <Route path='/contacts/view/:id' element={<ContactsView contacts={contacts} setContacts={setContacts}/>}/>
+          
         </Routes>
       </main>
     </>
