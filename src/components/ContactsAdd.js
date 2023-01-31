d6109366072dece203d3c1bff80f94d163460db2
CJ <illacj111213@gmail.com>
2023-01-31 09:02:32 +0000

    unfinished

--- src/components/ContactsAdd.js
+++ src/components/ContactsAdd.js
@@ -1,12 +1,20 @@
-import { useState } from "react"
-import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+
+const initialFormState = {
+  id: "",
+  firstName: "",
+  lastName: "",
+  street: "",
+  city: "",
+};
 
 function ContactsAdd(props) {
-
   // setContacts and contacts must be passed as props
   // to this component so new contacts can be added to the
   // state
-  const { setContacts, contacts } = props
+  const { setContacts, contacts } = props;
+  const navigate=useNavigate()
 
   //TODO: Implement controlled form
   //send POST to json server on form submit
@@ -15,25 +23,81 @@ function ContactsAdd(props) {
     <form className="form-stack contact-form">
       <h2>Create Contact</h2>
 
-      <label htmlFor="firstName">First Name</label>
-      <input id="firstName" name="firstName" type="text" required />
+      const handleChange=(event)=>{
+      const value=event.target.value;
+      const name=event.target.name;
+      const newFormState={...formState}
+      if (name==="firstName"){
+        newFormState.firstName=value
+      }
+      if(name==="lastName"){
+        newFormState.lastName=value
+      }
+      if(name==="street"){
+        newFormState.street=value
+      }
+      if(name==="city"){
+        newFormState.city=value
+      }
+      setFormState(newFormState)
+      }
+  
 
-      <label htmlFor="lastName">Last Name:</label>
-      <input id="lastName" name="lastName" type="text" required/>
 
-      <label htmlFor="street">Street:</label>
-      <input id="street" name="street" type="text" required/>
+    const handleSubmit = (event) => {
+        event.preventDefault()
+      console.log("Form Submitted")
+       const newContact = formState
+      const newContactJson = JSON.stringify(newContact)
+      
 
-      <label htmlFor="city">City:</label>
-      <input id="city" name="city" type="text" required/>
+     const options = {
+      method: "POST",
+      body: newContactJson,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+    fetch("http://localhost:4000/contacts", options)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Created New Contact", data)
 
-      <div className="actions-section">
-        <button className="button blue" type="submit">
-          Create
-        </button>
-      </div>
-    </form>
-  )
+        fetch("http://localhost:4000/contacts")
+          .then((res) => res.json())
+          .then((data) => {
+            setContacts(data)
+          })
+      })
+         navigate("/")
+  }
+
+  return (
+    <form className="form-stack contact-form" onSubmit={handleSubmit}>
+    <h2>Create Contact</h2>
+  
+    <label htmlFor="firstName">First Name</label>
+    <input id="firstName" name="firstName" type="text" required  onChange={handleChange}/>
+  
+    <label htmlFor="lastName">Last Name:</label>
+    <input id="lastName" name="lastName" type="text" required onChange={handleChange}/>
+  
+    <label htmlFor="street">Street:</label>
+    <input id="street" name="street" type="text" required onChange={handleChange}/>   
+    <label htmlFor="city">City:</label>
+  <input id="city" name="city" type="text" required onChange={handleChange}/>
+
+    <div className="actions-section">
+      <button className="button blue" type="submit">
+        Create
+      </button>
+    </div>
+  </form>
+
+)
 }
+  
 
-export default ContactsAdd
+  
+
+export default ContactsAdd;
--- src/components/ContactsView.js
+++ src/components/ContactsView.js
@@ -1,23 +1,37 @@
-import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function ContactsView() {
-  const [contact, setContact] = useState(false)
+  const [contact, setContact] = useState(false);
+  const [contact, setContact] = useState([]);
 
   //TODO: Get the contact to load from the params and fetch.
   //With useEffect, load the contact when params changes
   //and update contact state
-
+  const { id } = useParams();
   if (!contact) {
-    return <p>Loading</p>
+    return <p>Loading</p>;
   }
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/contacts/${id}`)
+      .then((res) => res.json())
+      .then((response) => {
+        console.log("conttct id", response);
+        setContact(response);
+      });
+  }, []);
 
   return (
     <div>
-      <h2>{contact.firstName} {contact.lastName}</h2>
-      <p>{contact.street} {contact.city}</p>
+      <h2>
+        {contact.firstName} {contact.lastName}
+      </h2>
+      <p>
+        {contact.street} {contact.city}
+      </p>
     </div>
-  )
+  );
 }
 
-export default ContactsView
+export default ContactsView;
