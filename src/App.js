388c1b3cd499b089da16e334c38ea8c14b2a98ed
Nikita Pieters <nikita_pieters@hotmail.com>
2023-04-24 13:59:08 +0200

    standard criteria and extension 3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import { useEffect, useState } from "react"
-import { Link, Route, Routes } from "react-router-dom"
-import ContactsList from "./components/ContactsList"
-import ContactsAdd from "./components/ContactsAdd"
-import ContactsView from "./components/ContactsView"
-import "./styles/styles.css"
+import { useEffect, useState } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import ContactsList from "./components/ContactsList";
+import ContactsAdd from "./components/ContactsAdd";
+import ContactsView from "./components/ContactsView";
+import "./styles/styles.css";
 
 export default function App() {
-  const [contacts, setContacts] = useState([])
-  
+  const [contacts, setContacts] = useState([]);
+
   //TODO: Load all contacts on useEffect when component first renders
+
+  useEffect(function () {
+    fetch("http://localhost:4000/contacts")
+      .then((res) => res.json())
+      .then((data) => setContacts(data));
+  }, []);
 
   return (
     <>
@@ -16,15 +22,27 @@ export default function App() {
         <h2>Menu</h2>
         <ul>
           {/* TODO: Make these links */}
-          <li>Contacts List</li>
-          <li>Add New Contact</li>
+          <li>
+            <Link to={"/"}> Contacts List</Link>
+          </li>
+          <li>
+            <Link to={"/contacts/add"}> Add New Contact</Link>
+          </li>
         </ul>
       </nav>
       <main>
         <Routes>
+          <Route path="/" element={<ContactsList contacts={contacts} />} />
+          <Route path="/contacts/:id" element={<ContactsView />} />
+          <Route
+            path="/contacts/add"
+            element={
+              <ContactsAdd contacts={contacts} setContacts={setContacts} />
+            }
+          />
           {/* TODO: Add routes here  */}
         </Routes>
       </main>
     </>
-  )
+  );
 }
--- src/components/ContactsAdd.js
+++ src/components/ContactsAdd.js
@@ -1,31 +1,79 @@
-import { useState } from "react"
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function ContactsAdd(props) {
-
   // setContacts and contacts must be passed as props
   // to this component so new contacts can be added to the
   // state
-  const { setContacts, contacts } = props
+  const { setContacts, contacts } = props;
+  const [newContact, setNewContact] = useState({
+    firstName: "",
+    lastName: "",
+    street: "",
+    city: "",
+  });
+  const navigate = useNavigate();
 
   //TODO: Implement controlled form
   //send POST to json server on form submit
+  const handleChange = (event) => {
+    setNewContact({ ...newContact, [event.target.name]: event.target.value });
+  };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const res = await fetch("http://localhost:4000/contacts", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newContact),
+    });
+    const data = await res.json();
+    setContacts([...contacts, data]);
+    navigate("/");
+  };
 
   return (
-    <form className="form-stack contact-form">
+    <form className="form-stack contact-form" onSubmit={handleSubmit}>
       <h2>Create Contact</h2>
 
       <label htmlFor="firstName">First Name</label>
-      <input id="firstName" name="firstName" type="text" required />
+      <input
+        id="firstName"
+        name="firstName"
+        type="text"
+        required
+        onChange={handleChange}
+        value={newContact.firstName}
+      />
 
       <label htmlFor="lastName">Last Name:</label>
-      <input id="lastName" name="lastName" type="text" required/>
+      <input
+        id="lastName"
+        name="lastName"
+        type="text"
+        required
+        onChange={handleChange}
+        value={newContact.lastName}
+      />
 
       <label htmlFor="street">Street:</label>
-      <input id="street" name="street" type="text" required/>
+      <input
+        id="street"
+        name="street"
+        type="text"
+        required
+        onChange={handleChange}
+        value={newContact.street}
+      />
 
       <label htmlFor="city">City:</label>
-      <input id="city" name="city" type="text" required/>
+      <input
+        id="city"
+        name="city"
+        type="text"
+        required
+        onChange={handleChange}
+        value={newContact.city}
+      />
 
       <div className="actions-section">
         <button className="button blue" type="submit">
@@ -33,7 +81,7 @@ function ContactsAdd(props) {
         </button>
       </div>
     </form>
-  )
+  );
 }
 
-export default ContactsAdd
+export default ContactsAdd;
