89983d2eb74600ac669e460c412492443b2e42a7
Nikita Pieters <nikita_pieters@hotmail.com>
2023-04-25 21:35:41 +0200

    done with the fitfh extension

--- src/App.js
+++ src/App.js
@@ -4,62 +4,91 @@ import ContactsList from "./components/ContactsList";
 import ContactsAdd from "./components/ContactsAdd";
 import ContactsView from "./components/ContactsView";
 import "./styles/styles.css";
+import "./styles/Loading.css";
 import ContactsEdit from "./components/ContactsEdit";
 import ContactsDelete from "./components/ContactsDelete";
 
 export default function App() {
   const [contacts, setContacts] = useState([]);
 
+  const [loading, setloading] = useState(undefined);
+  const [completed, setcompleted] = useState(undefined);
   //TODO: Load all contacts on useEffect when component first renders
 
-  useEffect(function () {
-    fetch("http://localhost:4000/contacts")
-      .then((res) => res.json())
-      .then((data) => setContacts(data));
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("http://localhost:4000/contacts")
+        .then((response) => response.json())
+        .then((json) => {
+          setContacts(json);
+          setloading(true);
+
+          setTimeout(() => {
+            setcompleted(true);
+          }, 1000);
+        });
+    }, 2000);
   }, []);
 
   return (
     <>
-      <nav className="menu">
-        <h2>Menu</h2>
-        <ul>
-          {/* TODO: Make these links */}
-          <li>
-            <Link to={"/"} className="menuLink">
-              {" "}
-              Contacts List
-            </Link>
-          </li>
-          <li>
-            <Link to={"/contacts/add"} className="menuLink">
-              {" "}
-              Add New Contact
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <main>
-        <Routes>
-          <Route path="/" element={<ContactsList contacts={contacts} />} />
-          <Route path="/contacts/:id" element={<ContactsView />} />
-          <Route
-            path="/contacts/add"
-            element={
-              <ContactsAdd contacts={contacts} setContacts={setContacts} />
-            }
-          />
-          <Route
-            path="/contacts/edit/:id"
-            element={<ContactsEdit setContacts={setContacts} />}
-          />
-          <Route
-            path="/contacts/delete/:id"
-            element={
-              <ContactsDelete setContacts={setContacts} contacts={contacts} />
-            }
-          />
-        </Routes>
-      </main>
+      {!completed ? (
+        <>
+          {!loading ? (
+            <div className="spinner">
+              <span className="text">Loading...</span>
+              <div className="half-spinner"></div>
+            </div>
+          ) : (
+            <div className="completed">&#x2713;</div>
+          )}
+        </>
+      ) : (
+        <>
+          <nav className="menu">
+            <h2>Menu</h2>
+            <ul>
+              <li>
+                <Link to={"/"} className="menuLink">
+                  {" "}
+                  Contacts List
+                </Link>
+              </li>
+              <li>
+                <Link to={"/contacts/add"} className="menuLink">
+                  {" "}
+                  Add New Contact
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          <main>
+            <Routes>
+              <Route path="/" element={<ContactsList contacts={contacts} />} />
+              <Route path="/contacts/:id" element={<ContactsView />} />
+              <Route
+                path="/contacts/add"
+                element={
+                  <ContactsAdd contacts={contacts} setContacts={setContacts} />
+                }
+              />
+              <Route
+                path="/contacts/edit/:id"
+                element={<ContactsEdit setContacts={setContacts} />}
+              />
+              <Route
+                path="/contacts/delete/:id"
+                element={
+                  <ContactsDelete
+                    setContacts={setContacts}
+                    contacts={contacts}
+                  />
+                }
+              />
+            </Routes>
+          </main>
+        </>
+      )}
     </>
   );
 }
