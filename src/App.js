133cc561eb32cab1296971a8aab82c0e788f7b65
Rose Breedveld <rosebreedveld@hotmail.com>
2023-04-25 13:42:37 -0400

    completed standard critera and some extensions

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ export default function App() {
   const [contacts, setContacts] = useState([])
   
   //TODO: Load all contacts on useEffect when component first renders
+  
+  useEffect(function() {
+    fetch("http://localhost:4000/contacts")
+      .then(res => res.json())
+      .then(data => setContacts(data))
+  }, [])
 
   return (
     <>
@@ -16,13 +22,21 @@ export default function App() {
         <h2>Menu</h2>
         <ul>
           {/* TODO: Make these links */}
-          <li>Contacts List</li>
-          <li>Add New Contact</li>
+          <li><Link to={'/'}>Contacts List</Link></li>
+          <li><Link to={'/contacts/add'}>Add New Contact</Link></li>
         </ul>
       </nav>
       <main>
         <Routes>
           {/* TODO: Add routes here  */}
+          // two attributes and in element attribute two components (info from stateHook)
+          // after the url / show the element of ContactsList
+          // there needs to be a front page (index '/') so see it as a index of a book.
+          // click the edit link you're opening a new form just like the contactAdd form. 
+          // so copying the page with a new url
+          <Route path='/' element={<ContactsList contacts={contacts} setContacts={setContacts}/>} />
+          <Route path='/contacts/:id' element={<ContactsView />} />
+          <Route path='/contacts/add' element={<ContactsAdd setContacts={setContacts} contacts={contacts} />} />
         </Routes>
       </main>
     </>
--- src/components/ContactsList.js
+++ src/components/ContactsList.js
@@ -1,10 +1,30 @@
 import { useState } from "react"
-import { Link, useSearchParams } from "react-router-dom"
+import { Link, useNavigate, useSearchParams } from "react-router-dom"
 
 function ContactsList(props) {
   
   //"contacts" must be passed as prop to this component
-  const { contacts } = props
+  const { contacts, setContacts } = props
+  // const contactDelete = (deleteId) => { 
+    
+  // }
+  // async it allows you to use the await keeper
+  // 
+  const contactDelete= async (e) => {
+    // const navigate = useNavigate()
+    const newContacts = contacts.filter((item) => item.id !== e.target.id);
+    setContacts(newContacts)
+    await fetch(`http://localhost:4000/contacts/${e.target.id}`, {
+     method: "DELETE",
+    });
+    // history.push("/");
+    // navigate("/") 
+    await fetch("http://localhost:4000/contacts")
+    .then((res) => res.json())
+    .then((data) => setContacts(data));
+   };
+
+  //  setContacts(newContacts);
 
   return (
     <>
@@ -21,8 +41,16 @@ function ContactsList(props) {
               </p>
               <p>
                 { /** TODO: Make a Link here to view contact */}
+                <Link to={`/contacts/${contact.id}`} >
+                {/* <Link to={`/contacts/add/${contact.id}`}> */}
+                {/* <link to={`/`} */}
                 View
+                </Link>
+                {/* // this function requires an argument */}
+                <button onClick={contactDelete} id = {contact.id}>Delete</button>
               </p>
+              {/* <p> <Link to={`/contacts/${contact.contactsDelete}`}>Delete</Link> 
+              </p> */}
             </li>
           )
         })}
