1b1023e76dfaaeae5fd6f2bc558132172697bb66
Max de Ruiter <m.j.deruiter99@hotmail.com>
2023-04-26 15:14:51 +0200

    Extension 7 finished

--- src/App.js
+++ src/App.js
@@ -13,48 +13,16 @@ import "./styles/styles.css";
 export default function App() {
   const [contacts, setContacts] = useState([]);
   const [loading, setLoading] = useState(false);
-  let [filterWork, setFilterWork] = useState(false);
-  let [filterPersonal, setFilterPersonal] = useState(false);
 
   useEffect(function () {
-    getContacts();
+    setLoading(true);
+    fetch("http://localhost:3030/contacts/")
+      .then((res) => res.json())
+      .then((data) => setContacts(data))
+      .then((e) => {
+        setLoading(false);
+      });
   }, []);
-
-  const getContacts = (e) => {
-    if (filterWork === true && filterPersonal === true) {
-      setLoading(true);
-      fetch("http://localhost:3030/contacts/?type=Work&type=Personal")
-        .then((res) => res.json())
-        .then((data) => setContacts(data))
-        .then((e) => {
-          setLoading(false);
-        });
-    } else if (filterWork === true && filterPersonal === false) {
-      setLoading(true);
-      fetch("http://localhost:3030/contacts/?type=Work")
-        .then((res) => res.json())
-        .then((data) => setContacts(data))
-        .then((e) => {
-          setLoading(false);
-        });
-    } else if (filterWork === false && filterPersonal === true) {
-      setLoading(true);
-      fetch("http://localhost:3030/contacts/?type=Personal")
-        .then((res) => res.json())
-        .then((data) => setContacts(data))
-        .then((e) => {
-          setLoading(false);
-        });
-    } else if (filterWork === false && filterPersonal === false) {
-      setLoading(true);
-      fetch("http://localhost:3030/contacts/")
-        .then((res) => res.json())
-        .then((data) => setContacts(data))
-        .then((e) => {
-          setLoading(false);
-        });
-    }
-  };
 
   return (
     <>
@@ -77,17 +45,7 @@ export default function App() {
         <Routes>
           <Route
             path="/"
-            element={
-              <ContactsList
-                loading={loading}
-                contacts={contacts}
-                setFilterWork={setFilterWork}
-                filterWork={filterWork}
-                setFilterPersonal={setFilterPersonal}
-                filterPersonal={filterPersonal}
-                getContacts={getContacts}
-              />
-            }
+            element={<ContactsList loading={loading} contacts={contacts} />}
           />
           <Route path="/contacts/:id" element={<ContactsView />} />
           <Route
--- src/components/ContactsList.js
+++ src/components/ContactsList.js
@@ -1,20 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useSearchParams } from "react-router-dom";
 
 function ContactsList(props) {
   //"contacts" must be passed as prop to this component
-  const { loading, contacts, getContacts } = props;
-  let { filterWork, filterPersonal } = props;
+  const { loading, contacts } = props;
 
-  const turnOnWorkFilter = (e) => {
-    props.setFilterWork((filterWork = !filterWork));
-    console.log("filterWork= ", filterWork, "filterPersonal= ", filterPersonal);
-    getContacts();
-  };
-  const turnOnPersonalFilter = (e) => {
-    props.setFilterPersonal((filterPersonal = !filterPersonal));
-    console.log("filterWork= ", filterWork, "filterPersonal= ", filterPersonal);
-    getContacts();
+  const [filter, setFilter] = useState(3);
+
+  let [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    if (searchParams.getAll("type").length === 0) {
+      searchParams.set("type", "Personal");
+      searchParams.append("type", "Work");
+      setSearchParams(searchParams);
+    }
+  }, [searchParams]);
+
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value);
+
+    switch (e.target.value) {
+      case "1":
+        searchParams.set("type", "Personal");
+        break;
+      case "2":
+        searchParams.set("type", "Work");
+        break;
+      case "3":
+        searchParams.set("type", "Personal");
+        searchParams.append("type", "Work");
+      default:
+        break;
+    }
+    setSearchParams(searchParams);
   };
 
   return (
@@ -22,10 +41,37 @@ function ContactsList(props) {
       <header>
         <h2>Contacts</h2>
       </header>
-      <h3>
-        Filter on <button onClick={turnOnWorkFilter}>💼</button> and/or{" "}
-        <button onClick={turnOnPersonalFilter}>🙍</button>
-      </h3>
+      <section className="contact-filter">
+        <span className="filter-span">
+          <input
+            type="radio"
+            id="personal"
+            name="type"
+            value={1}
+            checked={filter == 1}
+            onChange={handleFilterChange}
+          ></input>
+          <label htmlFor="personal">🙍</label>
+          <input
+            type="radio"
+            id="work"
+            name="type"
+            value={2}
+            checked={filter == 2}
+            onChange={handleFilterChange}
+          ></input>
+          <label htmlFor="work">💼</label>
+          <input
+            type="radio"
+            id="all"
+            name="type"
+            value={3}
+            checked={filter == 3}
+            onChange={handleFilterChange}
+          ></input>
+          <label htmlFor="all">All</label>
+        </span>
+      </section>
       <ul className="contacts-list">
         {loading ? (
           <div className="spinner-container">
@@ -34,22 +80,26 @@ function ContactsList(props) {
         ) : (
           <></>
         )}
-        {contacts.map((contact, index) => {
-          const { firstName, lastName, type } = contact;
-          return (
-            <li className="contact" key={index}>
-              <p>
-                {firstName} {lastName}
-                {type === "Work" ? <span>💼</span> : <span>🙍</span>}
-              </p>
-              <p>
-                <Link to={`/contacts/${contact.id}`}>View</Link>
-                <Link to={`/contacts/edit/${contact.id}`}>Edit</Link>
-                <Link to={`/contacts/delete/${contact.id}`}>Delete</Link>
-              </p>
-            </li>
-          );
-        })}
+        {contacts
+          .filter((contact) =>
+            searchParams.getAll("type").includes(contact.type)
+          )
+          .map((contact, index) => {
+            const { firstName, lastName, type } = contact;
+            return (
+              <li className="contact" key={index}>
+                <p>
+                  {firstName} {lastName}
+                  {type === "Work" ? <span>💼</span> : <span>🙍</span>}
+                </p>
+                <p>
+                  <Link to={`/contacts/${contact.id}`}>View</Link>
+                  <Link to={`/contacts/edit/${contact.id}`}>Edit</Link>
+                  <Link to={`/contacts/delete/${contact.id}`}>Delete</Link>
+                </p>
+              </li>
+            );
+          })}
       </ul>
     </>
   );
